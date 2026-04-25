885bba4861b1781f494fd7cd2da2610330d6b8b0
Ryan Paulo Magnaye <magnaye.rp@gmail.com>
2026-04-25 16:35:57 +0800

    index chanfe

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <h1 style="background-color:black">
 <?php
-echo "<h1>Hello CI/CD World, glad to be here!</h1>";
+echo "<h1>Hello CI/CD World, glad to be here! hjfsahijfashufsahhsfa</h1>";
 ?>
 </h1>
